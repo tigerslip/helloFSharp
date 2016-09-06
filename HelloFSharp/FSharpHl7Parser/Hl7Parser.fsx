c14f4872d92e90610892bd0c1aa3c3788bab47da
Joseph Evensen <JosephE@ellkay.com>
2016-09-06 17:10:14 -0400

    got a basic hl7 parser working

--- HelloFSharp/FSharpHl7Parser/Hl7Parser.fsx
+++ HelloFSharp/FSharpHl7Parser/Hl7Parser.fsx
@@ -9,24 +9,25 @@ let test parser str =
     | Failure(errorMsg, _, _) -> printfn "Failure: %s" errorMsg
 
 type Subcomponent = { value:string; }
-type Component = { subcomponents:Subcomponent[]; }
-type Field = { components:Component[]; }
-type Segment = { name:string; fields: Field[]; segments: Segment[] }
-type Hl7Message = { segments:Segment[] }
+type Component = { subcomponents:Subcomponent list; }
+type Field = { components:Component list; }
+type Segment = { name:string; fields: Field list; }
+type Hl7Message = { segments:Segment list }
 
 let hl7Seps = "|&~^"
-
-let str s = pstring s
-let psub = manyChars (noneOf hl7Seps) |>> (fun res -> {value = res})
-test psub "abc&123"
-
-let plines = sepBy (restOfLine false) newline
-
-let anyString = stringReturn 
-
-// split lines by
 let hl7 = "MSH|^~\&|A|B|C
 EVN|P03|1^2^3||
 PID|1||d2~e2~f2"
 
-test plines hl7
+let pSubcomponent = manyChars (noneOf "&^|") |>> (fun res -> {value = res})
+let pComponent = sepBy pSubcomponent (pstring "&") |>> (fun s -> {subcomponents = s})
+let pField = sepBy pComponent (pstring "^") |>> (fun c -> {components = c})
+let pHeader = anyString 3
+let pSegment = sepBy pField (pstring "|")
+
+let pMsg = pipe2 pHeader pSegment (fun name fields -> {name = name; fields = fields})
+
+test pHeader "MSH"
+test pComponent "abcd&cdef"
+test pField "^a&b^c"
+test pMsg "PID|a|b^c&d"
