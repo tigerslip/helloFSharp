e201fa13e983a4da78f0ff832890cf433a9c3d00
Joseph Evensen <JosephE@ellkay.com>
2016-09-06 17:47:37 -0400

    tried to start using discriminated unions, felt confused

--- HelloFSharp/FSharpHl7Parser/Hl7Parser.fsx
+++ HelloFSharp/FSharpHl7Parser/Hl7Parser.fsx
@@ -8,9 +8,11 @@ let test parser str =
     | Success(result, _, _) -> printfn "Success: %A" result
     | Failure(errorMsg, _, _) -> printfn "Failure: %s" errorMsg
 
-type Subcomponent = { value:string; }
+type Subcomponent =  { value:string; }
 type Component = { subcomponents:Subcomponent list; }
-type Field = { components:Component list; }
+type RepeatingField = {repitions:Component list list}
+type NormalField = { components:Component list; }
+type Field = NormalField | RepeatingField
 type Segment = { name:string; fields: Field list; }
 type Hl7Message = { segments:Segment list }
 
@@ -22,12 +24,18 @@ PID|1||d2~e2~f2"
 let pSubcomponent = manyChars (noneOf "&^|") |>> (fun res -> {value = res})
 let pComponent = sepBy pSubcomponent (pstring "&") |>> (fun s -> {subcomponents = s})
 let pField = sepBy pComponent (pstring "^") |>> (fun c -> {components = c})
+let pRepetion = sepBy pField (pstring "~") >>% (fun r -> {repitions = r})
 let pHeader = anyString 3
+let pNormalFieldOrRepitions = pipe2 pHeader (pField <|> pRepetion) |>> (fun name res -> match res with
+                                                            | NormalField -> {name = name; fields = res}
+                                                            | RepeatingField -> {name = name; fields = res})
 let pSegment = sepBy pField (pstring "|")
 
-let pMsg = pipe2 pHeader pSegment (fun name fields -> {name = name; fields = fields})
+//let pMsg = pipe2 pHeader pSegment (fun name fields -> {name = name; fields = fields})
 
 test pHeader "MSH"
 test pComponent "abcd&cdef"
 test pField "^a&b^c"
-test pMsg "PID|a|b^c&d"
+test pRepetion "a^b~b~c"
+
+//test pMsg "PID|a|b^c&d"
