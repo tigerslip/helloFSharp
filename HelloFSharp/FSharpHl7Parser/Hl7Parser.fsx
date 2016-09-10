6888a5d7d841f5570972337ef490e3e5c12658cc
tigerslip <JosephEvensen@hotmail.com>
2016-09-10 14:00:18 -0400

    having a hard time writing parsers to work with the discriminated types

--- HelloFSharp/FSharpHl7Parser/Hl7Parser.fsx
+++ HelloFSharp/FSharpHl7Parser/Hl7Parser.fsx
@@ -8,23 +8,27 @@ let test parser str =
     | Success(result, _, _) -> printfn "Success: %A" result
     | Failure(errorMsg, _, _) -> printfn "Failure: %s" errorMsg
 
-type Subcomponent = { value:string; }
-type Component = { subcomponents:Subcomponent list; }
-type Field = { components:Component list; }
-type Segment = { name:string; fields: Field list; }
-type Hl7Message = { segments:Segment list }
+type Component = Subcomponents of string list | Value of string
+type Field = Repetitions of Component list | Values of string list
+type Segment = { childSegments : Segment option; fields : Field list; }
+type Hl7Message = { segments : Segment list }
 
 let hl7Seps = "|&~^"
 let hl7 = "MSH|^~\&|A|B|C
 EVN|P03|1^2^3||
 PID|1||d2~e2~f2"
 
-let pSubcomponent = manyChars (noneOf "&^|") |>> (fun res -> {value = res})
-let pComponent = sepBy pSubcomponent (pstring "&") |>> (fun s -> {subcomponents = s})
-let pField = sepBy pComponent (pstring "^") |>> (fun c -> {components = c})
+let pval = manyChars (noneOf hl7Seps) |>> (fun res -> res)
+let getcomp vals = (fun vals -> match vals.Length with 
+                    | 0 -> {Value = ""}
+                    | 1 -> {Value = vals.Item[0]}
+                    | _ -> {Subcomponents = vals})
+
+let pcomp = sepBy pval (pstring "&") |>> (fun vals -> getcomp vals)
+
+let pfield = sepBy pcomp (pstring "^") |>> (fun r -> {Repitions = r}
 let pHeader = anyString 3
 let pSegment = sepBy pField (pstring "|")
-
 let pMsg = pipe2 pHeader pSegment (fun name fields -> {name = name; fields = fields})
 
 test pHeader "MSH"
