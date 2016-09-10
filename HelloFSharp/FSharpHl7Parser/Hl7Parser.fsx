37deb42199edbf2255ddd246df54287ca6ea3848
tigerslip <JosephEvensen@hotmail.com>
2016-09-10 15:04:15 -0400

    tried out sepby1 with discriminated unions

--- HelloFSharp/FSharpHl7Parser/Hl7Parser.fsx
+++ HelloFSharp/FSharpHl7Parser/Hl7Parser.fsx
@@ -18,20 +18,28 @@ let hl7 = "MSH|^~\&|A|B|C
 EVN|P03|1^2^3||
 PID|1||d2~e2~f2"
 
+// i think the parser sepBy1 will help here -> it only succeeds if there is at least one one item seperated
+
 let pval = manyChars (noneOf hl7Seps) |>> (fun res -> res)
-let getcomp vals = (fun vals -> match vals.Length with 
-                    | 0 -> {Value = ""}
-                    | 1 -> {Value = vals.Item[0]}
-                    | _ -> {Subcomponents = vals})
 
-let pcomp = sepBy pval (pstring "&") |>> (fun vals -> getcomp vals)
+let pcomp = sepBy pval (pstring "&") |>> (fun vals -> match vals.Length with 
+                                                        | 0 -> Value ""
+                                                        | 1 -> Value (vals.Item 0)
+                                                        | _ -> Subcomponents vals)
 
-let pfield = sepBy pcomp (pstring "^") |>> (fun r -> {Repitions = r}
-let pHeader = anyString 3
-let pSegment = sepBy pField (pstring "|")
-let pMsg = pipe2 pHeader pSegment (fun name fields -> {name = name; fields = fields})
+test pcomp "a&b&c"
+let ok =  sepBy1 pval (pstring "^")
 
-test pHeader "MSH"
-test pComponent "abcd&cdef"
-test pField "^a&b^c"
-test pMsg "PID|a|b^c&d"
+test ok ""                  
+
+//let pfield = sepBy pcomp (pstring "^") |>> (fun c -> List.map c (fun vals -> match vals with 
+//                                                                                | Subcomponents -> 
+//                                                        
+//let pHeader = anyString 3
+//let pSegment = sepBy pField (pstring "|")
+//let pMsg = pipe2 pHeader pSegment (fun name fields -> {name = name; fields = fields})
+
+//test pHeader "MSH"
+//test pComponent "abcd&cdef"
+//test pField "^a&b^c"
+//test pMsg "PID|a|b^c&d"
