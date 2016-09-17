513c86d92ae1b9d33f44d30d74e2b6fa645fee19
tigerslip <JosephEvensen@hotmail.com>
2016-09-16 22:15:45 -0400

    consolidated some functions

--- HelloFSharp/FSharpHl7Parser/Hl7Parser.fsx
+++ HelloFSharp/FSharpHl7Parser/Hl7Parser.fsx
@@ -39,11 +39,13 @@ let escapedChar = attempt (pchar '\\' >>. anyChar |>> unescape .>> skipChar '\\'
 
 let pHl7Element = manyChars (normalChar <|> escapedChar)
 
-let pcomp = sepBy pHl7Element (pchar '&') |>> (fun vals -> List.mapi (fun i s -> {value = s; position = i}) vals)
+let pHl7Part c p result = sepBy p (pchar c) |>> result
 
-let pfield = sepBy pcomp (pchar '^') |>> (fun comps -> List.mapi (fun i c -> {subcomponents = c; position = i}) comps)
+let pcomp = pHl7Part '&' pHl7Element (fun vals -> List.mapi (fun i s -> {value = s; position = i}) vals)
 
-let pRepsOrField = sepBy pfield (pchar '~') |>> (fun fields -> match fields.Length with
+let pfield = pHl7Part '^' pcomp (fun comps -> List.mapi (fun i c -> {subcomponents = c; position = i}) comps)
+
+let pRepsOrField = pHl7Part '~' pfield (fun fields -> match fields.Length with
                                                                 | 0 | 1 -> SingleField {components = fields.Item 0; position = 0}
                                                                 | _ -> Repetitions (List.mapi (fun i c -> {components = c; position = i}) fields))
 
@@ -52,5 +54,8 @@ let pheader = anyString 3 |>> (fun name -> name)
 let pSegment = pipe2 pheader (sepBy pRepsOrField (pchar '|')) (fun name repsOrFields -> {name = name; fields = repsOrFields})
 
 test pSegment "EVN|A&1^B^C|123~456~789"
+
+let thing = notEmpty (pstring "" |>> (fun s -> s))
+test thing ""
 
 let hl7 = "MSH|^~\\&|A|B|C\nEVN|P03|1^2^3&4&5||\nPID|1||d2~e2~f2"
