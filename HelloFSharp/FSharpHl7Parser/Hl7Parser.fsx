6ec9e01a882c6ad76cf7348ba703099a36c97b6c
Joseph Evensen <JosephE@ellkay.com>
2016-09-19 18:33:19 -0400

    ugh unsaved changes

--- HelloFSharp/FSharpHl7Parser/Hl7Parser.fsx
+++ HelloFSharp/FSharpHl7Parser/Hl7Parser.fsx
@@ -43,12 +43,21 @@ let test parser str =
 type result = {value:string; index:int}
 type otherThing = {results:result list; index:int}
 
+let delims = "|^"
 
-let pipeData = "|A||B|C"
 let pipeandcarrotdata = "|A^B^C||B|C"
+
+let zipi res = List.mapi (fun i item -> res i item)
+let zipPipes = zipi (fun i t -> {results = t; index = i})
+let zipCarrots = zipi (fun i t -> {value = t; index = i})
+
+let cleanlist predicate = List.filter (fun i -> predicate i)
+let cleanPipes = cleanlist (fun t -> t.results.IsEmpty <> true)
+let cleanCarrots = cleanlist (fun t -> t.value <> "")
 
 let collect items = List.mapi (fun i item -> {value = item; index = i}) items
 let cleanEmpties items = List.filter (fun item -> item.value <> "") items
 
-let pPipes = sepBy (manyChars (noneOf "|")) (pstring "|") |>> (collect)
-test pPipes pipeData
+let pCarrots = sepBy (manyChars (noneOf delims)) (pstring "^") |>> (zipCarrots >> cleanCarrots)
+let pPipes = sepBy pCarrots (pstring "|") |>> (zipPipes >> cleanPipes)
+test pPipes pipeandcarrotdata
