0a25b897d46b9c0a00a22a4e04d37359892b5f02
Joseph Evensen <JosephE@ellkay.com>
2016-09-20 13:35:05 -0400

    added segment parser

--- HelloFSharp/FSharpHl7Parser/Hl7Parser.fsx
+++ HelloFSharp/FSharpHl7Parser/Hl7Parser.fsx
@@ -11,9 +11,10 @@ let test parser str =
 type Subcomponent = {value:string; index:int}
 type Component = {subcomponents:Subcomponent list; index:int}
 type Field = {components:Component list; index:int}
-type Repetitions = {fields:Field list; index:int}
-type FieldOrRepetitions = Field of Field | Repetitions of Repetitions
-type Segment = {name:string; fields:FieldOrRepetitions list; children:Segment list; index:int}
+type Repetitions = {fs:Field list; index:int}
+//type FieldOrRepetitions = Field of Field | Repetitions of Repetitions
+//type Segment = {name:string; fields:FieldOrRepetitions list; children:Segment list; index:int}
+type Segment = {name:string; fields:Field list}
 
 let delims = "|^"
 
@@ -37,21 +38,23 @@ let zipi res = List.mapi (fun i item -> res i item)
 let zipSubs = zipi (fun i s -> {value = s; index = i})
 let zipComps = zipi (fun i s -> {subcomponents = s; index = i})
 let zipFields = zipi (fun i c -> {components = c; index = i})
-let zipReps = zipi (fun i f -> {fields=f; index=i})
-let zipSegs = zipi (fun i f n c -> {name = n; fields = f; index = i; children = c}) 
+//let zipReps = zipi (fun i f -> {fs=f; index=i})
+//let zipSegs = zipi (fun i f n c -> {name = n; fields = f}) 
 
 let clean predicate = List.filter (fun i -> predicate i)
 let cleanSubs = clean (fun t -> t.value <> "")
 let cleanlist selector = clean (fun l -> (List.isEmpty (selector l)) <> true)
 let cleanComps = cleanlist (fun c -> c.subcomponents)
 let cleanFields = cleanlist (fun f -> f.components)
-let cleanReps = cleanlist (fun r -> r.fields)
+//let cleanReps = cleanlist (fun r -> r.fields)
 
 let pHl7Part s p zip clean = sepBy p (pstring s) |>> (zip >> clean)
 let psubs = pHl7Part "&" pHl7Element zipSubs cleanSubs
 let pcomps = pHl7Part "^" psubs zipComps cleanComps
-let preps = pHl7Part "~" pcomps 
+//let preps = pHl7Part "~" pcomps (zipFields >> cleanFields) (zipReps >> cleanReps)
 let pfields = pHl7Part "|" pcomps zipFields cleanFields
 
+let pname = anyString 3 |>> id
+let pseg = pipe2 pname pfields (fun name fields -> {name = name; fields = fields})
 
-//test pSegment "EVN|A&1^B^C|123~456~789"
+test pseg "EVN|A&1^B^C|123~456~789"
