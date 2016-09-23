16ab824851c724492681c855b3d475883debc102
tigerslip <JosephEvensen@hotmail.com>
2016-09-22 22:56:52 -0400

    got fields and reps working

--- HelloFSharp/FSharpHl7Parser/Hl7Parser.fsx
+++ HelloFSharp/FSharpHl7Parser/Hl7Parser.fsx
@@ -11,7 +11,7 @@ let test parser str =
 type Subcomponent = {value:string; index:int}
 type Component = {subcomponents:Subcomponent list; index:int}
 type Field = {components:Component list}
-type Repetitions = {repetitions:list<Component list>}
+type Repetitions = {repetitions:Component list list}
 type FieldOrRepetitions = Field of Field * index:int | Repetitions of Repetitions * index:int
 type Segment = {name:string; fields:FieldOrRepetitions list; children:Segment list}
 type Hl7Message = {segments: Segment list}
@@ -32,9 +32,9 @@ let pHl7Element = manyChars (normalChar <|> escapedChar)
 
 let zipSubs = List.mapi (fun i s -> {value = s; index = i})
 let zipComps = List.mapi (fun i s -> {subcomponents = s; index = i})
-let zipFields = List.mapi (fun i c -> {components = c; index = i})
-let zipReps = (fun c -> {repetitions=c})
-//let zipSegs = zipi (fun i f n c -> {name = n; fields = f}) 
+let zipRepsOrFields = List.mapi (fun i fields -> match fields with
+                                                    | [f] -> Field({components = fields.Item 0}, i)
+                                                    | _ -> Repetitions({repetitions = fields}, i))
 
 let cleanSubs = List.filter (fun t -> t.value <> "")
 let cleanlist selector = List.filter (fun l -> (List.isEmpty (selector l)) <> true)
@@ -44,11 +44,11 @@ let cleanFields = cleanlist (fun f -> f.components)
 let pHl7Part s p zipclean = sepBy p (pstring s) |>> zipclean
 let psubs = pHl7Part "&" pHl7Element (zipSubs >> cleanSubs)
 let pcomps = pHl7Part "^" psubs (zipComps >> cleanComps)
-let preps = pHl7Part "~" pcomps zipReps
-let pfields = pHl7Part "|" preps (zipFields >> cleanFields)
+let pReps = sepBy pcomps (pstring "~")
+let pRepsOrFields = sepBy pReps (pstring "|") |>> zipRepsOrFields
 
 let pname = anyString 3 |>> id
-let pseg = pipe2 pname pfields (fun name fields -> {name = name; fields = fields; segments = List.empty})
+let pseg = pipe2 pname pRepsOrFields (fun name fields -> {name = name; fields = fields; children = List.empty})
 let pmsg = sepBy pseg newline |>> (fun segments -> {segments = segments})
 
 test pmsg "EVN|A&1^B^C|123~456~789\r\nPID|A|B|C"
